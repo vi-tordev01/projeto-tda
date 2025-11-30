54da7bb31078649d5cf62a17393296e2130e94c4
vi-tordev01 <vitor.jampa28@gmail.com>
2025-11-30 12:25:28 -0300

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int numeros[5] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; i++){
+        printf("%d ", numeros[i]);
+    }
+
+    return 0;
+}
---
+++ strings.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char nome[5] = "Joao";
+    printf("%s", nome);
+
+    return 0;
+}
